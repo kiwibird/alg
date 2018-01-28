1fa707c77db4417173424b4898044b03beb5869d
kiwibird <gykgod@163.com>
2018-01-28 14:56:08 +0800

    remove some old code

--- main.cpp
+++ main.cpp
@@ -1,14 +1,7 @@
-#include <stdlib.h>
 #include <stdio.h>
-#include "utils\utils.h"
-#include "sort\sort.h"
 
 int main()
 {
-	int a[10] = {0,3,4,1,5,8,7,9,6,2};
-	selectsort(a, 10);
-	printarray(a, 10);
-	
 	printf("hello world first test!\n");
 	return 0;
 }
