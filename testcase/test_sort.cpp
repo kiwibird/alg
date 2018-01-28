30e9b1c80aa0095c5658152487543557a8d72a69
kiwibird <gykgod@163.com>
2018-01-28 16:32:31 +0800

    insert sort

--- testcase/test_sort.cpp
+++ testcase/test_sort.cpp
@@ -1,9 +1,23 @@
 #include "../utils/utils.h"
 #include "../sort/sort.h"
 
+TEST(bubblesort)
+{
+	int a[10] = {0,3,5,1,4,8,7,9,6,2};
+	bubblesort(a, 10);
+	printarray(a, 10);
+}
+
 TEST(selectsort)
 {
 	int a[10] = {0,3,4,1,5,8,7,9,6,2};
 	selectsort(a, 10);
+	printarray(a, 10);
+}
+
+TEST(insertsort)
+{
+	int a[10] = {0,3,4,1,5,8,7,9,6,2};
+	insertsort(a, 10);
 	printarray(a, 10);
 }
